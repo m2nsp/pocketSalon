b10506d050e79ed0bf15e428f7a25cafc2236231
m2nsp <mini071663@gmail.com>
2024-10-04 14:55:48 +0900

    [미션2] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/pocketSalon/miniproject/MiniprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.pocketSalon.miniproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniprojectApplication>(*args)
+}
---
+++ src/test/kotlin/com/pocketSalon/miniproject/MiniprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pocketSalon.miniproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiniprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
