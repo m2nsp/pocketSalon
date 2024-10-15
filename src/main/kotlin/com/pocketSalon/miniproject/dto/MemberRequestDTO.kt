85b2d86e7b8d65defaf14463e4d08e0242eaf4da
m2nsp <mini071663@gmail.com>
2024-10-16 00:02:30 +0900

    fix: add constructor

--- src/main/kotlin/com/pocketSalon/miniproject/dto/MemberRequestDTO.kt
+++ src/main/kotlin/com/pocketSalon/miniproject/dto/MemberRequestDTO.kt
@@ -1,8 +1,18 @@
 package com.pocketSalon.miniproject.dto
 
-data class MemberRequestDTO (
+import com.pocketSalon.miniproject.domain.entity.Member
+
+data class MemberRequestDTO(
     val id: String,
     val password: String,
     val phoneNumber: String,
     val name: String
-)
+) {
+    // Member 엔티티를 사용하여 DTO 생성자
+    constructor(member: Member) : this(
+        id = member.id,
+        password = member.password,  // 엔티티에서 패스워드 가져오기
+        phoneNumber = member.phoneNumber,  // 엔티티에서 전화번호 가져오기
+        name = member.name  // 엔티티에서 이름 가져오기
+    )
+}
