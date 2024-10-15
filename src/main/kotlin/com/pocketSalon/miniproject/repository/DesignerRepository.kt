009e029878e95fecef8473497c6679e52f443b63
m2nsp <mini071663@gmail.com>
2024-10-15 22:21:10 +0900

    feat: add function findByDesignerId

--- src/main/kotlin/com/pocketSalon/miniproject/repository/DesignerRepository.kt
+++ src/main/kotlin/com/pocketSalon/miniproject/repository/DesignerRepository.kt
@@ -1,8 +1,9 @@
 package com.pocketSalon.miniproject.repository
 
 import com.pocketSalon.miniproject.domain.entity.Designer
+import com.pocketSalon.miniproject.domain.entity.Reservation
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface DesignerRepository : JpaRepository<Designer, String> {
-
+    fun findByDesignerId(designerId: String): List<Reservation>
 }
