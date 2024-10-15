c03f3272b8609783db66c7d1b36ab58fc0eaed9a
m2nsp <mini071663@gmail.com>
2024-10-15 22:21:29 +0900

    feat: add fields

--- src/main/kotlin/com/pocketSalon/miniproject/domain/entity/Designer.kt
+++ src/main/kotlin/com/pocketSalon/miniproject/domain/entity/Designer.kt
@@ -2,6 +2,9 @@ package com.pocketSalon.miniproject.domain.entity
 
 import com.fasterxml.jackson.databind.ser.Serializers.Base
 import com.pocketSalon.miniproject.domain.constant.specialty
+import com.pocketSalon.miniproject.dto.DesignerDTO
+import com.pocketSalon.miniproject.dto.ReviewResponseDTO
+import com.pocketSalon.miniproject.repository.DesignerRepository
 import jakarta.persistence.*
 
 @Entity
@@ -9,15 +12,22 @@ class Designer(
     name: String,
     phoneNumber: String,
     introduction: String,
-    specialty: specialty
+
+    @Enumerated(EnumType.STRING)
+    var specialty: specialty // specialty 필드
 ):BaseEntity() {
     @Id
     @Column(name = "D_id", nullable = false)
     var D_id: String = ""
+
+    var name: String = name
+    var phoneNumber: String = phoneNumber
+    var introduction: String = introduction
 
     @OneToMany(mappedBy = "designer", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
     var reservations: List<Reservation> = mutableListOf()
 
     @OneToMany(mappedBy = "designer", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
     var reviews: List<Review> = mutableListOf()
+
 }
